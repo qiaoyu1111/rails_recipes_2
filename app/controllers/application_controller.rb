84c498151b15446b0a9c395aab71fc3a89b0eb15
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-15 18:05:08 +0800

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,13 @@ class ApplicationController < ActionController::Base
 
     I18n.locale = session[:locale] || I18n.default_locale
   end
+
+  before_action :set_timezone
+
+  def set_timezone
+    if current_user && current_user.time_zone
+      Time.zone = current_user.time_zone
+    end
+  end
+
 end
