5a64be660c29f8ade7ef81c6d02e73ebf16f6fbf
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-17 18:39:09 +0800

    add

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :events
-
   namespace :admin do
     root "events#index"
-    resources :events
+    resources :events do
+      resources :tickets, :controller => "event_tickets"
+        collection do
+          post :bulk_update
+        end
+    end
     resources :users do
        resource :profile, :controller => "user_profiles"
      end
@@ -17,5 +20,7 @@ Rails.application.routes.draw do
   get "/faq" => "pages#faq"
 
   resource :user
+
+
 
 end
