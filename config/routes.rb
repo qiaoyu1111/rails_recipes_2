e7aa810c438985e94ba1b3bc2b046df6c8974c69
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-23 14:42:50 +0800

    implement Wizards

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,23 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :events do
-    resources :registrations
+    resources :registrations do
+      member do
+        get "steps/1" => "registrations#step1", :as => :step1
+        patch "steps/1/update" => "registrations#step1_update", :as => :update_step1
+        get "steps/2" => "registrations#step2", :as => :step2
+        patch "steps/2/update" => "registrations#step2_update", :as => :update_step2
+        get "steps/3" => "registrations#step3", :as => :step3
+        patch "steps/3/update" => "registrations#step3_update", :as => :update_step3
+      end
+    end
   end
 
   namespace :admin do
     root "events#index"
-    resources :events
+    resources :events do
+      resources :tickets, :controller => "event_tickets"
+    end
     resources :users do
        resource :profile, :controller => "user_profiles"
      end
