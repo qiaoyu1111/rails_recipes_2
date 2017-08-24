3e91a0f13820418bc874aed5216870ba9e0eb741
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-24 17:12:48 +0800

    add

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -6,6 +6,41 @@ class Admin::EventsController < AdminController
 
   def show
     @event = Event.find_by_friendly_id!(params[:id])
+
+    colors = ['rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
+               'rgba(255, 206, 86, 0.2)',
+               'rgba(75, 192, 192, 0.2)',
+               'rgba(153, 102, 255, 0.2)',
+               'rgba(255, 159, 64, 0.2)'
+               ]
+
+     ticket_names = @event.tickets.map { |t| t.name }
+
+     status_colors = { "confirmed" => "#FF6384",
+                      "pending" => "#36A2EB"}
+
+    @data1 = {
+        labels: ticket_names,
+        datasets: Registration::STATUS.map do |s|
+          {
+            label: I18n.t(s, :scope => "registration.status"),
+            data: @event.tickets.map{ |t| t.registrations.by_status(s).count },
+            backgroundColor: status_colors[s],
+            borderWidth: 1
+          }
+        end
+    }
+
+     @data2 = {
+        labels: ticket_names,
+        datasets: [{
+            label: '# of Amount',
+            data:  @event.tickets.map{ |t| t.registrations.by_status("confirmed").count * t.price },
+            backgroundColor: colors,
+            borderWidth: 1
+        }]
+    }
   end
 
   def new
@@ -54,11 +89,11 @@ class Admin::EventsController < AdminController
       if params[:commit] == I18n.t(:bulk_update)
         event.status = params[:event_status]
         if event.save
-          total += 1
+          total  = 1
         end
       elsif params[:commit] == I18n.t(:bulk_delete)
         event.destroy
-        total += 1
+        total  = 1
       end
     end
 
